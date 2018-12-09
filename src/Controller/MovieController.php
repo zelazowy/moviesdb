bb919f094ef0564b6a65934254344a8daf8d2717
Jonczyk <krzysztof.jonczyk@gmail.com>
2018-12-09 18:11:49 +0100

    Add movie details action and template
    Change knp_paginator version to dev-master to get materialize pagination template

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -6,6 +6,7 @@ use App\Entity\Movie;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,11 +16,20 @@ class MovieController extends AbstractController
      * {@inheritdoc}
      * @Route("/{page}", name="movie_list", defaults={"page"=1})
      */
-    public function listController(EntityManagerInterface $entityManager, PaginatorInterface $paginator, int $page = 1): Response
+    public function listAction(EntityManagerInterface $entityManager, PaginatorInterface $paginator, int $page = 1): Response
     {
         $moviesQuery = $entityManager->getRepository(Movie::class)->getAllQuery();
         $pagination = $paginator->paginate($moviesQuery, $page);
 
         return $this->render('Movie/list.html.twig', ['pagination' => $pagination]);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @Route("/movie/{id}", name="movie_details")
+     */
+    public function detailsAction(Request $request, Movie $movie): Response
+    {
+        return $this->render('Movie/details.html.twig', ['movie' => $movie, 'page' => $request->query->get('page', 1)]);
     }
 }
